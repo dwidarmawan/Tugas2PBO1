a3b3c12deee34511356769f0774ebdcd25edcff4
dwidarmawan <81225366+dwidarmawan@users.noreply.github.com>
2021-06-21 05:37:26 -0700

    Membuat class dan method

---
+++ src/BiodataSaya.java
@@ -0,0 +1,24 @@
+import java.util.Date;
+
+public class BiodataSaya {
+    private String namaLengkap;
+    private String tempatLahir;
+    private Date tanggalLahir;
+    private String Alamat;
+    private String Status;
+    private String Pekerjaan;
+    private String Agama;
+    private int Usia;
+
+    public void pekerjaan(){
+        System.out.println("PT.Telekomunikasi");
+    }
+
+    public void tugas(){
+        System.out.println("Mencari Customer");
+    }
+
+    
+
+}
+
